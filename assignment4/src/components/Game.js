b69167817481d7b6048d9992bb3533d7985c1c07
Ilma Ismail <139671858+IlmaIsmail@users.noreply.github.com>
2024-11-23 10:15:36 +0000

    getRandomeAnimal and handleAnimalClick

--- assignment4/src/components/Game.js
+++ assignment4/src/components/Game.js
@@ -5,6 +5,19 @@ import { useState } from "react";
 export default function Game(){
     const [targetAnimal, setTargetAnimal] = useState(getRandomAnimal());
     const [result, setResult] = useState(null);
+
+    function getRandomAnimal(){
+        const randomIndex = Math.floor(Math.random()*animals.length);
+        return animals[randomIndex];
+    }
+
+    const handleAnimalClick = (id) =>{
+        if(id === targerAnimal.id){
+            setResult("Win");
+        } else {
+            setResult("Lose");
+        }
+    };
 }
 
 
